bd4147bce6415591fe4ee3cdd71c000ca9a9860a
TwinkieMan <38349179+TwinkleMan@users.noreply.github.com>
2020-05-31 22:15:33 +0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/StructLab9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StructLab9.dir/main.c.obj"
+  "StructLab9.exe"
+  "StructLab9.exe.manifest"
+  "StructLab9.pdb"
+  "libStructLab9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StructLab9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/haha.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/haha.dir/lect-12-02.c.obj"
+  "haha.exe"
+  "haha.exe.manifest"
+  "haha.pdb"
+  "libhaha.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/haha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
